bd32cc4534e38ad6d932f2377dc3786148ab9bc2
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-09-20 18:29:01 +0300

    рефакторинг

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,11 @@ const listItems = document.querySelector('.listItems');
 const allCaloriesText = document.querySelector('.listTitle_calories');
 const chart = document.querySelector(".chart");
 const totalCalories = document.querySelector(".totalCaloriesDay");
-const bar =  chart.querySelectorAll('.bar');
+const bar = chart.querySelectorAll('.bar');
 const deleteAll = document.querySelector(".deleteAll");
 const warningsDaily = document.querySelector(".warningsDailyLimit");
 const sort = document.querySelector(".sort");
-const filterInput = document.querySelector(".filterInput")
+const filterInput = document.querySelector(".filterInput");
 
 const addProductsBtn = addProduct.querySelector('button');
 const addTargetCaloriesBtn = addTargetCalories.querySelector("button");
@@ -17,50 +17,57 @@ let arrProducts = JSON.parse(localStorage.getItem('arrProducts')) || [];
 let allCalories = JSON.parse(localStorage.getItem('allCalories')) || 0;
 let totalCaloriesDay = JSON.parse(localStorage.getItem('totalCaloriesDay')) || 0;
 
+const createListItemElement = (item, index) => {
+    const li = document.createElement("li");
+    li.textContent = `${item.product}  ${Number(item.calories)}`;
+    const btnDelete = document.createElement('button');
+    btnDelete.textContent = "Удалить";
+    btnDelete.className = "delete-marker-button";
+    li.append(btnDelete);
+    listItems.append(li);
+    btnDelete?.addEventListener("click", () => {
+        deleteListItem(index);
+    });
+};
+
+const updateLocalStorage = () => {
+    localStorage.setItem('arrProducts', JSON.stringify(arrProducts));
+    localStorage.setItem('allCalories', JSON.stringify(allCalories));
+    localStorage.setItem('totalCaloriesDay', JSON.stringify(totalCaloriesDay));
+};
 
 const updateCalories = () => {
     allCalories = arrProducts.reduce((total, item) => total + Number(item.calories), 0);
-    localStorage.setItem('allCalories', JSON.stringify(allCalories));
     updateCaloriesText();
     updateCaloriesBar();
+    updateLocalStorage();
+    warningsDailyLimit();
 };
 
 const createListItems = () => {
     listItems.innerHTML = '';
     arrProducts.forEach((item, index) => {
-        const li = document.createElement("li");
-        li.textContent = `${item.product}  ${Number(item.calories)}`;
-        const btnDelete = document.createElement('button');
-        btnDelete.textContent = "Удалить";
-        btnDelete.className = "delete-marker-button";
-        li.append(btnDelete)
-        listItems.append(li)
-        btnDelete?.addEventListener("click",() => {
-            deleteListItem(index);
-        });
-    })
-    updateCaloriesText();
-    updateCaloriesBar();
-    localStorage.setItem('allCalories', JSON.stringify(allCalories));
-    warningsDailyLimit()
+        createListItemElement(item, index);
+    });
     updateCalories();
-}
+};
+
 const warningsDailyLimit = () => {
-  if (allCalories > totalCaloriesDay) {
-      warningsDaily.textContent = "Вы привысили дневную норму";
-      warningsDaily.style.color = "red"
-  }
-}
+    if (allCalories && totalCaloriesDay && allCalories > totalCaloriesDay) {
+        warningsDaily.textContent = "Вы превысили дневную норму";
+        warningsDaily.style.color = "red";
+    }
+};
+
 const deleteListItem = (index) => {
-    arrProducts.splice(index,1);
-    localStorage.setItem('arrProducts', JSON.stringify(arrProducts));
+    arrProducts.splice(index, 1);
     createListItems();
-}
+};
+
 addProductsBtn.addEventListener('click', () => {
     const inputs = addProduct.querySelectorAll('input');
-    const newItem = {product: inputs[0].value, calories: inputs[1].value};
+    const newItem = { product: inputs[0].value, calories: inputs[1].value };
     arrProducts.push(newItem);
-    localStorage.setItem('arrProducts', JSON.stringify(arrProducts));
     createListItems();
     updateCaloriesBar();
     inputs[0].value = '';
@@ -69,9 +76,9 @@ addProductsBtn.addEventListener('click', () => {
 
 addTargetCaloriesBtn.addEventListener("click", () => {
     const input = addTargetCalories.querySelector('input');
-    totalCaloriesDay = Number(input.value)
-    localStorage.setItem('totalCaloriesDay', JSON.stringify(totalCaloriesDay));
+    totalCaloriesDay = Number(input.value);
     updateCaloriesText();
+    updateLocalStorage();
 });
 
 deleteAll.addEventListener("click", () => {
@@ -79,7 +86,7 @@ deleteAll.addEventListener("click", () => {
     bar[0].style.height = '0%';
     arrProducts = [];
     allCaloriesText.textContent = `Всего калорий: 0`;
-    localStorage.setItem('arrProducts', JSON.stringify(arrProducts));
+    updateLocalStorage();
 });
 
 const sortByDeadline = (e) => {
@@ -94,13 +101,13 @@ const sortByDeadline = (e) => {
             return a.calories - b.calories;
         }
     });
-    createListItems()
+    createListItems();
 };
 
-sort.addEventListener("click", sortByDeadline)
+sort.addEventListener("click", sortByDeadline);
 
 const updateCaloriesText = () => {
-    totalCalories.textContent = `Цель калорий на день: ${totalCaloriesDay}`
+    totalCalories.textContent = `Цель калорий на день: ${totalCaloriesDay}`;
     if (totalCaloriesDay !== undefined && totalCaloriesDay > 0) {
         bar[1].style.height = `100%`;
     } else {
@@ -110,30 +117,23 @@ const updateCaloriesText = () => {
 
 const updateCaloriesBar = () => {
     allCaloriesText.textContent = `Всего калорий: ${allCalories}`;
-   const percent = (allCalories / totalCaloriesDay) * 100
+    const percent = (allCalories / totalCaloriesDay) * 100;
     bar[0].style.height = `${percent}%`;
 };
+
 const filterItems = (e) => {
     const filter = e.target.value;
-    const filterArr = arrProducts.filter((item) => {return item.product.indexOf(filter) !== -1})
+    const filterArr = arrProducts.filter((item) => item.product.indexOf(filter) !== -1);
     listItems.innerHTML = '';
     filterArr.forEach((item, index) => {
-        const li = document.createElement("li");
-        li.textContent = `${item.product}  ${Number(item.calories)}`;
-        const btnDelete = document.createElement('button');
-        btnDelete.textContent = "Удалить";
-        btnDelete.className = "delete-marker-button";
-        li.append(btnDelete)
-        listItems.append(li);
-        btnDelete?.addEventListener("click",() => {
-            deleteListItem(index);
-        });
-    })
-}
+        createListItemElement(item, index);
+    });
+};
 
-if(arrProducts) {
+if (arrProducts) {
     createListItems();
     updateCaloriesText();
     updateCaloriesBar();
 }
-filterInput.addEventListener("input", filterItems)
+
+filterInput.addEventListener("input", filterItems);
